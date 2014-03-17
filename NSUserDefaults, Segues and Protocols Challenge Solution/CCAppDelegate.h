6ecf9a90ce72b4417923a6a6fbd14a0892af1843
Ray Louie <raylouie@Raymonds-MacBook-Air-2.local>
2014-03-17 14:31:43 -0700

    Initial Commit

---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Raymond Louie on 3/17/14.
+//  Copyright (c) 2014 Raymond Louie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Raymond Louie on 3/17/14.
+//  Copyright (c) 2014 Raymond Louie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
